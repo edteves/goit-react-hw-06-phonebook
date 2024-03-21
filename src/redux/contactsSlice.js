7a96e8d3addc32ca3dacb1064b85a49697458645
edteves <edwinjrteves29@gmail.com>
2024-03-21 17:14:40 +0800

    final code

---
+++ src/redux/contactsSlice.js
@@ -0,0 +1,52 @@
+import { createSlice, nanoid } from '@reduxjs/toolkit';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const contactSlice = createSlice({
+  name: 'contacts',
+  initialState: {
+    initalContacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+  },
+
+  reducers: {
+    addContact: {
+      reducer: (state, action) => {
+        state.initalContacts.push(action.payload);
+      },
+
+      prepare: (name, number) => {
+        return {
+          payload: {
+            id: nanoid(),
+            name: name.trim(),
+            number: number.trim(),
+          },
+        };
+      },
+    },
+    deleteContact: (state, action) => {
+      const index = state.initalContacts.findIndex(
+        contact => contact.id === action.payload
+      );
+      state.initalContacts.splice(index, 1);
+    },
+  },
+});
+
+const persistConfig = {
+  key: 'contacts',
+  storage,
+};
+
+export const contactsReducer = persistReducer(
+  persistConfig,
+  contactSlice.reducer
+);
+
+export const { addContact, deleteContact, deleteAllContacts } =
+  contactSlice.actions;
